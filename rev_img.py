782ace4fabcf31b0dabb2e54cc665b3ef7396d87
pratyushraunak <pratyushraunak1@gmail.com>
2018-01-25 19:19:43 +0530

    Update rev_img.py

--- rev_img.py
+++ rev_img.py
@@ -1,24 +1,29 @@
-
+#######################################################################################################################
+# Pratyush Raunak												       #
+# Python Automation 												       #
+# Fixing the reversed bytes and nibbles to construct an image.							       #
+# 														       #
+########################################################################################################################
 
 def read_file():
-	with open("reverse", "rb") as binary_file:
+	with open("reverse", "rb") as binary_file: #opening the file to read as byte
 		data = binary_file.read()
 		data = bytearray(data)
-		data = data[::-1]
-		rev_nibble(data)
+		data = data[::-1] #reversing the entire byte sequence
+		rev_nibble(data) #function call
 def rev_nibble(data):
-	 swap_data=''
+	 swap_data='' #intializing a variable
 	 for i in data:
-		low_nibble = i & 0x0F
-    		high_nibble = i & 0xF0
-    		result = (high_nibble >> 4) | (low_nibble << 4)
+		low_nibble = i & 0x0F #getting low nibble value
+    		high_nibble = i & 0xF0 #getting high nibble value 
+    		result = (high_nibble >> 4) | (low_nibble << 4) #swapping the nibbles and joining the nibble as one byte
 
 		swap_data+=chr(result)
 		out_file(swap_data)
 
 
 def out_file(swap_data):
-	with open("my_rev_img.png", "wb") as outfile:
+	with open("my_rev_img.png", "wb") as outfile: #writing bytes to a file
 		 outfile.write(str(swap_data))
 
 read_file()
